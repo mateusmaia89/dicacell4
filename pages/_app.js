0ea5fad43cc84c8505b963fb8183a3e6de1f7b1e
Mateus Maia <mateusmaia@MacBook-Pro-de-Mateus.local>
2025-08-10 22:36:03 -0300

    Tema fixo roxo/laranja e hover forte nas cards; remove fundo reativo

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,5 @@
 import '../styles/globals.css';
-import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    const onMove = (e) => {
-      const x = e.clientX / window.innerWidth;
-      const y = e.clientY / window.innerHeight;
-      document.documentElement.style.setProperty('--mx', `${x*100}%`);
-      document.documentElement.style.setProperty('--my', `${y*100}%`);
-    };
-    window.addEventListener('mousemove', onMove, { passive: true });
-    onMove({ clientX: window.innerWidth*0.7, clientY: window.innerHeight*0.3 });
-    return () => window.removeEventListener('mousemove', onMove);
-  }, []);
   return <Component {...pageProps} />;
 }
